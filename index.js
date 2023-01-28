9152b712d3b4ee6b177cfd8a2cc2cec3694e7581
Razibul Islam <razibulislam665@gmail.com>
2023-01-28 16:57:03 +0600

    post method done

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.5jjbyfi.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  const PostCollection = client.db("helpRoom").collection("posts");
+  app.post("/post", async (req, res) => {
+    const query = req.body;
+    const result = await PostCollection.insertOne(query);
+    res.send(result);
+  });
+}
+run().catch((err) => console.error(err));
+
+app.get("/", (req, res) => {
+  res.send("I am from helpRoom");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
